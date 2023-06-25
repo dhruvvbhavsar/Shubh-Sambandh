763ca43dc954222bcb5564b339322502935cff8c
Dhruv Bhavsar <dhruv.bhavsar75@gmail.com>
2023-06-25 16:24:40 +0530

    blogs | faqs

---
+++ src/routes/Blogs/secrets-of-happy-married-life-pt1/+page.svelte
@@ -0,0 +1,142 @@
+<section class="bg-[#111827] py-12">
+	<article class="text-white flex flex-col justify-center items-center">
+		<h1 class="text-4xl underline underline-offset-4 text-center">
+			Secrets of a happy married life, Part 1
+		</h1>
+		<section class="max-w-[65ch] mt-8 space-y-4 mx-4">
+			<p>
+				“Enough is enough! It’s been two years; I can’t take it anymore. Divorce seems the only way
+				out. But when I think of the kids I get cold feet”. Vinay said this, his eyes brimming with
+				tears.
+			</p>
+			<img
+				src="https://media.gettyimages.com/id/1144130467/photo/i-cant-believe-were-fighting-about-this-again.jpg?s=612x612&w=gi&k=20&c=Qs7cc9qI0UQIjuJsOSJuQyyd9Y4O0WebJQuDg7zmHxM="
+				alt=""
+			/>
+			<p>
+				This is the story of my dear friend Vinay. Vinay and Reema had married 10 years ago with a
+				lot of pomp and fair. They were fond of each other since college. Initially, the families
+				were not ok with the alliance, but their love convinced all. It took almost four years, but
+				they finally got married with everyone’s blessings. On their wedding day, their happiness
+				knew no bounds. And why not, that day had come after so many struggles. They had an ordinary
+				married life. They had two lovely children and were a picture of marital bliss and harmony.
+				Over the years Vinay and I had managed to stay in touch and speak occasionally.
+			</p>
+			<p>
+				But recently due to time constraints and because of our very different jobs, he being an
+				army man and I part of the corporate race, we hadn’t spoken for quite some time. A few weeks
+				ago I traveled to Vinay’s city and managed to catch up with him. The two of us had a lot to
+				discuss, from the good old days of college to our busy current lives. Vinay seemed a little
+				sad when speaking about his family and he happened to mention that he was considering
+				separating from his wife.
+			</p>
+			<p>
+				The first few years were full of love and happiness, but as the years wore on their
+				relationship lost love and respect. Their behavior towards each other changed as did
+				expectations. The small disagreements turned into big arguments every day. Vinay felt Reema
+				didn’t love him anymore. She changed from an attentive caring woman to a dissatisfied
+				complaining one. Initially, Vinay didn’t realize what was happening, but soon her behavior
+				told him she wasn’t happy with him. According to him, he tried everything to better the
+				situation but after the daily fights and arguments, divorce seems to be the only solution.
+			</p>
+			<img
+				src="https://media.gettyimages.com/id/1198174856/photo/young-couple-arguing-while-having-problems-in-their-relationship.jpg?s=612x612&w=gi&k=20&c=KZ9z3GmhzS7Rqz2SXXwn_PWwp4Rj1PMt4zn0lTcLRUc="
+				alt=""
+			/>
+			<p>
+				There are many couples like Vinay and Reema, who start off the marriage very happy but the
+				matters take a turn for the worse, often ending up in divorce. In India marriage was once
+				thought of as a sacred commitment between two souls not only for the current life but for
+				seven births. Today these relationships are very delicate and seem to not last even a few
+				years. Divorce is seen as the only and best way to handle not so perfect marriages. Marital
+				disharmony, fights, and divorces have become so commonplace today. The essence of love and
+				sacrifice in relationships is eroding away. All relationships need love to survive, as does
+				marriage.
+			</p>
+			<p>
+				If there is no love, there is no bond and the relationship weakens. Love is the secret
+				ingredient. It’s like sugar that sweetens any drink. Traditionally in Indian society the
+				woman is deemed to be the thread that binds all relationships. She is expected to embody
+				love, sacrifice, and commitment required to maintain the varied relationships that co-exist
+				in a family setup. If a man is in charge of work and providing for the family, the woman is
+				in charge of keeping the family together.
+			</p>
+			<p>
+				Whether its two people who decide to marry after having a long relationship or an arranged
+				marriage where everything is done after matching horoscopes and family details, problems
+				affect every marriage. In the early years, all seems rosy as the couple concentrates on each
+				other’s good traits. But soon the small things that they don’t like about each other start
+				bothering them. And in some cases, these things blow up in proportion where the husband and
+				wife can’t stand each other. According to a national survey, only 1.2 % of the couples
+				interviewed said that they have never fought
+			</p>
+			<p>
+				So the question arises what changes a loving happy relationship into one where there is
+				hate, disrespect, and contempt between the spouses. Sometimes to this extent that the person
+				seems blind to even the good things in his/her spouse. According to some psychologists, if
+				you put the physical relationship aside, marriage and friendship are very similar in nature.
+				we have many and different kinds of friendships. Our relationship with each friend is
+				different. Some friends you go out for movies with, some friends help you at the workplace
+				and sometimes some friends like family, help you in good and bad times
+			</p>
+			<img
+				src="https://media.gettyimages.com/id/148193507/photo/happy-young-couple-playing-against-blue-sky.jpg?s=612x612&w=gi&k=20&c=4RzaEj2CwlpJ3a0DjLob8VV1VRa3Cp-CGE3ECJFdQEE="
+				alt=""
+			/>
+			<p>
+				After marriage when you treat your spouse as your best friend, you are looking at him/her to
+				replace so many of your different friends. That leads to the biggest problem. We expect one
+				person to be a replacement for so many. We expect too much. No marriage is perfect. Problems
+				will arise, but how equipped and ready we are to handle the challenges makes all the
+				difference. If tackled with maturity and understanding all relationships can be saved.
+			</p>
+			<p>
+				The keyword for couples trying to survive marriage is 'compromise'. Both have to be ready to
+				adjust a little. Be it what food to eat for dinner, or what to watch on television? What
+				work is important? From the biggest to the smallest decisions made together as a couple each
+				individual might have a strong opinion. Often when the individual wants to make independent
+				decisions and not take into consideration the partners wish the relationship starts going
+				downhill.
+			</p>
+			<p>
+				Spouses that are mature and thoughtful and work to lessen the stress caused in their
+				relationships lead more content lives. Prevention of these feelings of discontent and ego is
+				the best way to maintain cordial relationships. If you can see the long term picture and not
+				react to everything about your spouse that you don’t like, you find it easier to love and
+				respect your spouse and your marriage
+			</p>
+			<img
+				src="https://media.gettyimages.com/id/1309328855/vector/husband-holding-umbrella-over-wife-with-shopping-bags-in-rain.jpg?s=612x612&w=gi&k=20&c=qs5iODzXagNsd0pDxdmxTCU-VvpHI9XKAC7nqgwAkvY="
+				alt=""
+			/>
+			<p>
+				According to traditional Indian culture, love is not mental or physical compatibility. Love
+				refers to selfless service. If the wife’s love is dependent on how much money husband gives
+				her, or the man’s love is dependent on how well she cooks, it is not love, it is an
+				arrangement, a deal. Love in its purest form is selfless. The man’s happiness should lie in
+				the woman’s happiness and vice versa. Even if in giving the partner that happiness you go
+				through some stress.
+			</p>
+			<p>
+				If in a marriage one partner sacrifices and the other only takes that leads to exploitation.
+				Both people involved have to give and take. And that’s why the selfless love where your love
+				for your partner makes you put them first is the only love that can survive. Enthusiastic
+				but selfish love never lasts. Marriages that succeed need mature and selfless love. That is
+				the only way to a happy married life.
+			</p>
+		</section>
+		<div class="flex flex-col gap-4 mt-20 justify-start items-center">
+			<img
+				src="http://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEhwzd4A5Gpq86ILfb8yKs7TXehUHkvjPYFcIHG0qlX3aecmEaSmMMFvsCBRoMT7iVc3q2X8yuRXu5tzgNHIix0XgNcNUb1cdBpifbz4sqd8Dpyp5mUPP67zMlkVgkuN0p8/s113/Passport+Size.jpg"
+				alt="posted by"
+			/>
+			<p class="text-2xl">Sunil Thakur</p>
+			<p class="max-w-[65ch] mx-4">
+				I believe that the root cause of any problem currently exist is misdirected mind. There is a
+				great need of purification of our consciousness. Until we work on root cause any effort
+				would be on superficial level only. Our vedic literatures have enormous potentials to reform
+				the misdirected civilization today.
+			</p>
+		</div>
+	</article>
+</section>
